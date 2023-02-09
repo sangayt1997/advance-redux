ff83c018a51d33d2cb9fbf14bdf0a53dc04c5767
Sangay Thinley <thinley.sangay@selise.ch>
2023-02-09 13:39:19 +0600

    sending cart data in firebase database

--- src/app.js
+++ src/app.js
@@ -2,9 +2,18 @@ import Cart from './components/cart/cart/cart';
 import Layout from './components/layout/layout';
 import Products from './components/shop/products/products';
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
     const showCart = useSelector(state => state.ui.isCartVisible);
+    const cart = useSelector(state => state.cart);
+
+    useEffect(() => {
+        fetch('https://order-food-eb0b9-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+            method: 'PUT',
+            body: JSON.stringify(cart),
+        });
+    }, [cart])
 
     return (
         <Layout>
